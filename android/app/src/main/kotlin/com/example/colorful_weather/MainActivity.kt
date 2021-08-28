ac71036169fd0af6716cb12d53bbc6e4e6362119
Jhonatan Galeano <galeanoj03@gmail.com>
2021-08-28 08:52:02 -0500

    init project

---
+++ android/app/src/main/kotlin/com/example/colorful_weather/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.colorful_weather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:colorful_weather/src/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Weather',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
